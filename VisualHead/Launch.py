8e16e258de84b30580fb7d03701d3be3ec16684a
rakshit1991 <rakshitdayal.allamraju@gmail.com>
2016-02-27 11:36:36 -0600

    changed launch.py for visual head

--- VisualHead/Launch.py
+++ VisualHead/Launch.py
@@ -18,23 +18,47 @@ from threading import Thread
 # Command with shell expansion
 
 
-def main():
-	start_ros = True # pass as input later
-	launch_delay = 30 # Input in future version
+def main(argv):
+	start_ros = False # pass as input later
+	launch_delay = 600 #launch time delay b/n 2 uavs # Input in future version
 
 	if start_ros:
 		t = Process(target=startcore, args=())
 		t.start()
 		sleep(10)
+
+
+	# Parse the arguments for positioning the uavs at desired locations
+	latitude_args = argv[0].split('=')[1].split(',')
+	longitude_args = argv[1].split('=')[1].split(',')
+	latitude_list = map(float,latitude_args)
+	longitude_list = map(float,longitude_args)
 	
+	if len(latitude_list) != len(longitude_list):
+		print "Mismatch number of input arguments"
+		sys.exit(-1)
+
 	print "Starting flightgear visual head ..."
 
 	controller_hosts = {} ##dictionary with key as IP and values as (server_id,no of instances)
-	controller_hosts["139.78.78.169"] = (1,2)	
-	#controller_hostIP = "139.78.78.169" # IP address of server running the controller ### input in future version
+	controller_hosts["139.78.78.169"] = (1,3)	#(server_id,no of instances)
+	
+	# Compute the total instances of uav thats have been defined
+	total_instances = 0
+	for i in xrange(len(controller_hosts.values())):
+		total_instances = total_instances + controller_hosts.values()[i][1]
+	
+	# Check if input arguments are equal to number of instances defined
+	if len(latitude_list) != total_instances:
+		print "Mismatch number of latitude values and total instances"
+		sys.exit(-1)
+	
+	if len(longitude_list) != total_instances:
+		print "Mismatch number of longitude values and total instances"
+		sys.exit(-1)
+
 
 	jobs_fg = []
-	
 	for controller_hostIP in controller_hosts.keys():	
 		response = os.system("ping -c 1 " + controller_hostIP)
 		if response == 0:
@@ -72,12 +96,13 @@ def launch_qgc(server_id, instance):
 	callsign = Instance_info['callsign']
 	mav_port = Instance_info['mavlink_port']
 	C = callsign[-2:]
+	print callsign + ' opened in QGC'
 	qgc_str = './fgqgclink localhost 15555 '+str(C)+' ' +str(mav_port)
 	os.system(qgc_str)
-	print callsign + ' opened in QGC'
+	
 
 	
 	
 
 if __name__ == '__main__':	
-	main()
+	main(sys.argv[1:])
