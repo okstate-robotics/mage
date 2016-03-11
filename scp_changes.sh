41f3d7a2e865315b15b6bdfb6c0cd0e6defcc768
S. M. Al Mahi <al-mahi@users.noreply.github.com>
2016-03-11 14:37:16 -0600

    Update scp_changes.sh

--- scp_changes.sh
+++ scp_changes.sh
@@ -2,6 +2,7 @@
 
 ## Send to daslab-server 1
 # Send the matlab files
+# TODO File Name changes
 scp ~/Rakshit/mage-dev/ros/{ch4.mat,compressGP.m,config.txt,Env_Init.m,ExpandNFuse.m,exploringPI.m,extract_ch4.m,GPGridWorld.m,Init.m,init_test.m,kernel.m,onlineGP.m,PolicyIteration.m,ros_env.sh,Rot.m,Sensor_info.m,testros.m} rakshit@daslab-server1:~/Rakshit/ros/
 # copy the python libraries
 scp -r ~/Rakshit/mage-dev/ros/src/mage/libraries rakshit@daslab-server1:~/Rakshit/ros/src/mage
